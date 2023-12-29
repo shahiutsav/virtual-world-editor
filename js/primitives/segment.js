a25b3aa0d020610ddaa9a0c37bccd1d49c5c28fe
shahiutsav <92775677+shahiutsav@users.noreply.github.com>
2023-12-29 10:47:20 +0545

    Improve graph editor with modern implementation
    This commit introduces modern interaction system to the graph editor.
    Previously, there was no graph editor, just canvas and buttons were used
    to add and remove points and segments randomly. This approach was not
    suitable for the final product.
    
    Now, a graph editor is introduced that lets the user add points and
    segments using mouse clicks. The user can select points, and drag it
    wherever they desire. They can unselect those points and segments are
    created when a point is already selected.
    
    Overall, the user interface is now more interactive and easy to use.

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -12,12 +12,14 @@ class Segment {
         return this.p1.equals(point) || this.p2.equals(point);
     }
 
-    draw(ctx, width = 2, color = 'black') {
+    draw(ctx, { width = 2, color = 'black', dash = [] } = {}) {
         ctx.beginPath();
         ctx.strokeStyle = color;
+        ctx.setLineDash(dash);
         ctx.lineWidth = width;
         ctx.moveTo(this.p1.x, this.p1.y);
         ctx.lineTo(this.p2.x, this.p2.y);
         ctx.stroke();
+        ctx.setLineDash([]);
     }
 }
